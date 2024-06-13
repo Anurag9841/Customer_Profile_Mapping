b2a9b37a2c9c85d42ac41c9a8af6c844f1aa78dc
Anurag Karki <anuragkarkikarki79@gmail.com>
2024-06-13 13:19:08 +0545

    customer_mapping_edited

--- dags/customer_mapping.py
+++ dags/customer_mapping.py
@@ -7,6 +7,7 @@ from pyspark.sql.functions import *
 from pyspark.sql.types import *
 from pyspark.sql.window import *
 import json
+import pandas as pd
 def mapping():
     spark = SparkSession.builder.appName("spark_dataframe_py").getOrCreate()
     url = "jdbc:mysql://172.25.0.1:3306/extenso_config"
@@ -44,26 +45,52 @@ def mapping():
         top_product = [row['product_name'] for row in top_10]
         return top_product
 
-    def mapping(product_category_map, rw_transaction_data):
+    def map(product_category_map, rw_transaction_data):
         joined = rw_transaction_data.join(product_category_map, ['product_id', 'product_type_id', 'module_id'])
-        joined = joined.withColumn("months", month(col("last_modified_date")))
+        joined = joined.withColumn("first_day_of_month", trunc(col("last_modified_date"), "month"))
         top_product = most_used_product(joined)
         filtered_df = joined.filter(col("product_name").isin(top_product))
-        product_used_count = filtered_df.groupBy("payer_account_id", "product_name").pivot("months").count().fillna(0)
+        product_used_count = filtered_df.groupBy("payer_account_id", "product_name").pivot("first_day_of_month").count().fillna(0)
         columns = product_used_count.columns
+        start_date = datetime.strptime(columns[2], '%Y-%m-%d').date()
+        end_date = datetime.strptime(columns[-1], '%Y-%m-%d').date()
+        current_date = start_date
+        date_list_comp = []
+        while current_date <= end_date:
+            date_list_comp.append(current_date.strftime('%Y-%m-%d'))
+            current_date = current_date + timedelta(days=32 - current_date.day)
+        modified_dates = [date_str[:8] + '01' for date_str in date_list_comp]
+        additional_Date = []
+        for date in modified_dates:
+            if date not in columns:
+                additional_Date.append(date)
+        if len(additional_Date)!=0: 
+            data = {cols: [0] * product_used_count.count() for cols in additional_Date}
+            df = pd.DataFrame(data)
+            additional_df = spark.createDataFrame(df)
+            window = Window.orderBy(monotonically_increasing_id())
+            additional_df = additional_df.withColumn("id", row_number().over(window) - 1)
+            product_used_count = product_used_count.withColumn("id", row_number().over(window) - 1)
+            combined_df = product_used_count.join(additional_df, on="id", how="inner").drop("id")
+            excluded_columns = ['payer_account_id', 'product_name']
+            coln_combined = excluded_columns + modified_dates + [str(end_date)]
+            combined_df = combined_df.select(coln_combined)
+        else:
+            combined_df = product_used_count
+        columns = combined_df.columns
         monthly_columns = columns[2:]
         for column in monthly_columns:
-            product_used_count = product_used_count.withColumn(column, when(col(column) > 0, 1).otherwise(col(column)))
+            combined_df= combined_df.withColumn(column, when(col(column) > 0, 1).otherwise(col(column)))
         concat_expr = col(monthly_columns[0])
         for col_name in monthly_columns[1:]:
             concat_expr = concat(concat_expr, col(col_name))
-        product_used_count = product_used_count.withColumn("used_map",concat_expr)
-        return product_used_count.write.csv('./output_file/final_table_customer_mapping', header=True,mode='overwrite')
+        combined_df = combined_df.withColumn("used_map",concat_expr)
+        return combined_df.write.csv('./output_file/final_table_customer_mapping', header=True,mode='overwrite')
 
     start_date,last_date = get_start_last_transaction_date()
     product_category_map = table("product_category_map",start_date,last_date)
     rw_transaction_data = table("rw_transaction_data",start_date,last_date)
-    mapping(product_category_map,rw_transaction_data)
+    map(product_category_map,rw_transaction_data)
 
 default_args = {
     'owner': 'airflow',
